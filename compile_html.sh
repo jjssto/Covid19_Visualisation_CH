c8220cb5e85e9df7e9f6775d17a8d3163084a499
jjssto <jjssto@posteo.de>
2020-04-11 16:10:44 +0200

    minor changes

--- compile_html.sh
+++ compile_html.sh
@@ -2,7 +2,7 @@
 
 TMP="analyse_tmp.pmd"
 
-cd data/covid_19
+cd /var/www/python/data/covid_19
 git pull
 cd ../..
 
@@ -10,3 +10,8 @@ cd ../..
 sed "s/\[Timestamp\]/$(date)/g" analyse.pmd > $TMP
 
 pweave -f md2html -F figures -o corona_info.html $TMP
+
+cp corona_info.html ../html/documents/
+cp figures/* ../html/documents/figures/
+chmod go+r ../html/documents/corona_info.html
+chmod go+r ../html/documents/figures/*
