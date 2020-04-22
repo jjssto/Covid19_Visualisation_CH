9c1656aeeff3e09fa6ee4424fbc9854126105980
jjssto (server) <jjssto@posteo.de>
2020-04-22 04:24:20 +0200

    "Automatic Update"

--- create_html.py
+++ create_html.py
@@ -83,7 +83,7 @@ class Create_html:
         f = open( self.output_file, 'r')
         t = open( self.tracking_file, 'r')
         for line in f:
-            if re.match(r'.*\<HEAD.*',line) is not None:
+            if re.match(r'.*HEAD.*',line) is not None:
                 tmp_file.write( line )
                 for line_ in t:
                     tmp_file.write(line_)
